4d41589b3fe01a724c262aa1c3bb064cab9d0dcf
Adeolu05 <peluoladavid123@gmail.com>
2026-02-17 17:23:06 +0100

    fix: restore useEffect import in VerifyPage (used by timer)

--- src/pages/VerifyPage.tsx
+++ src/pages/VerifyPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AuthLayout } from "../components/auth/AuthLayout";
 import { OTPInput } from "../components/auth/OTPInput";
 import { NumericKeypad } from "../components/auth/NumericKeypad";
